cd7f0ac1f9d5174bb33f1a8d22bbe9fada2ad936
jd <jdaly183@gmail.com>
2021-01-17 18:44:44 -0500

    Add Tab Render Ordering, Addition of Network Tab

--- app/src/pages/pagemgr.lua
+++ app/src/pages/pagemgr.lua
@@ -1,14 +1,24 @@
 require "pages._systemstatus"
+require "pages._network"
 
 PageManager = {
 	Configuration = nil,
-	Network = nil,
+	Network = Network_Page,
 	Power = nil,
 	Production = nil,
 	Storage = nil,
 	System_Status = System_Status_Page,
 	Transport = nil,
 
+}
+PageManagerTabOrder = {
+	"System_Status",
+	"Network",
+	"Production",
+	"Storage",
+	"Power",
+	"Transport",
+	"Configuration"
 }
 --function PageManager:CreatePage(event,num,func,layout,navbarlink)
 --	local page = func(event,num,layout,navbarlink)
